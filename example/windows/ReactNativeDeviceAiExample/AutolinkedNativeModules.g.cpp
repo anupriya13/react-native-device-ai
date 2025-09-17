b44ab9c4d5be492f221662379c87f24eecd3d54f
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-09-17 14:01:22 +0530

    autolink

--- example/windows/ReactNativeDeviceAiExample/AutolinkedNativeModules.g.cpp
+++ example/windows/ReactNativeDeviceAiExample/AutolinkedNativeModules.g.cpp
@@ -3,12 +3,16 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-device-ai
+#include <winrt/ReactNativeDeviceAi.h>
+
 namespace winrt::Microsoft::ReactNative
 {
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
-    UNREFERENCED_PARAMETER(packageProviders);
+    // IReactPackageProviders from react-native-device-ai
+    packageProviders.Append(winrt::ReactNativeDeviceAi::ReactPackageProvider());
 }
 
 }
